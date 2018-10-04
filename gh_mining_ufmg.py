060403240d96e4195ac9c7d0e01c132fabc0431b
jacksonpradolima <jacksonpradolima@gmail.com>
2018-10-04 16:33:01 -0300

    add mining refactorings from datasets

---
+++ gh_mining_ufmg.py
@@ -0,0 +1,17 @@
+import utils.GitHubUtils as GitHubUtils
+import utils.JsonUtils as JsonUtils
+
+# Get the dataset in json format
+dataset = JsonUtils.get_data("data/refactorings.json")
+
+# Filter unique repositories
+repos = set([GitHubUtils.get_slug_git_url(x.repository) for x in dataset])
+
+# Filter the repositories that contains Travis CI
+repos_travis = [x for x in repos if GitHubUtils.is_travis_project(x)]
+
+# Filter refactorings where the repositoris contains Travis CI
+dataset_travis = [x for x in dataset if GitHubUtils.get_slug_git_url(
+    x.repository) in repos_travis]
+
+JsonUtils.save_data("data/refactorings_ufmg_travis.json", dataset_travis)
