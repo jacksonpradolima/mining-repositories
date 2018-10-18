c2912d2aebdcbce8f1477d577ebceebec65bf823
jacksonpradolima <jacksonpradolima@gmail.com>
2018-10-18 08:38:44 -0300

    command to run build analysis

--- run_buildlog_analysis.sh
+++ run_buildlog_analysis.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-ls raw_data/buildlogs | parallel -j 10 ruby ../travistorrent-tools/bin/buildlog_analysis.rb "raw_data/buildlogs/{}"
+#ls /mnt/NAS/japlima/mining-repositories/raw_data/buildlogs | parallel -j 10 ruby /mnt/NAS/japlima/travistorrent-tools/bin/buildlog_analysis.rb "/mnt/NAS/japlima/mining-repositories/raw_data/buildlogs/{}"
+
+ls /mnt/NAS/japlima/mining-repositories/raw_data/buildlogs | parallel -j 10 ruby bin/buildlog_analysis.rb "/mnt/NAS/japlima/mining-repositories/raw_data/buildlogs/{}"
